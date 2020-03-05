3684e836b4579bf7382df0abbeea4a19c7b3c332
anil <anil@robosoftin.com>
2020-03-05 10:10:24 +0530

    Updates

---
+++ precache-manifest.501993732a8322fcb67c10ab2542cf4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ff1fa06d6e1d99d43b3f42d20b9e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b443d81008bba4493951",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21c1913e8235ba03c66b",
+    "url": "/static/js/2.8226fa0b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8226fa0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b443d81008bba4493951",
+    "url": "/static/js/main.4da96df1.chunk.js"
+  },
+  {
+    "revision": "1988077964fc992e348f",
+    "url": "/static/js/runtime-main.4296755d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
